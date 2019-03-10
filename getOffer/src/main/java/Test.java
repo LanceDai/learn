45baf5e10e913264c23eda3f3bc0cf27259455d5
LanceDai <603842325@qq.com>
2019-03-10 23:00:07 +0800

    2019年03月10日

--- getOffer/src/main/java/Test.java
+++ getOffer/src/main/java/Test.java
@@ -2,41 +2,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 class Test {
     public static void main(String[] args) {
-        System.out.println(new B().getValue());
-    }
-
-    static class A {
-        protected int value;
-
-        public A(int v) {
-            setValue(v);
-        }
-
-        public void setValue(int value) {
-            this.value = value;
-        }
-
-        public int getValue() {
-            try {
-                value++;
-                return value;
-            } finally {
-                this.setValue(value);
-                System.out.println(value);
-                ConcurrentHashMap
-            }
-        }
-    }
-
-    static class B extends A {
-        public B() {
-            super(5);
-            setValue(getValue() - 3);
-        }
-
-        @Override
-        public void setValue(int value) {
-            super.setValue(2 * value);
-        }
+        String str = new String("abc");
+        System.out.println("str = " + str);
     }
 }
