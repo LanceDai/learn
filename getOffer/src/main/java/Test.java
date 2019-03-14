79db1b4b868691facf02b6d7397dbc9f66690fb9
LanceDai <603842325@qq.com>
2019-03-14 23:00:03 +0800

    2019年03月14日

--- getOffer/src/main/java/Test.java
+++ getOffer/src/main/java/Test.java
@@ -1,7 +1,6 @@
 import javax.script.SimpleBindings;
 import javax.swing.plaf.TableHeaderUI;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -14,12 +13,38 @@ class Test {
     public static void main(String[] args) {
         String str = new String("abc");
         System.out.println("str = " + str);
-        LinkedHashMap linkedHashMap = new LinkedHashMap(SIZE){
+        LinkedHashMap linkedHashMap = new LinkedHashMap(SIZE) {
             @Override
             protected boolean removeEldestEntry(Map.Entry eldest) {
                 return (size() + 1 > SIZE);
             }
         };
-        AbstractQueuedSynchronizer
+        AbstractQueuedSynchronizer abstractQueuedSynchronizer;
+        HashMap hashMap;
+        ConcurrentHashMap concurrentHashMap;
+        String string;
+        TreeMap treeMap = new TreeMap();
+        SortedMap sortedMap;
+        Object o = Singleton.SINGLETON.getInstance();
+//        Integer.hashCode();
+//        Long.hashCode();
+//        Double.hashCode();
+//        Short.hashCode();
+//        Character.hashCode();
+//        Byte.hashCode();
+//        Boolean.hashCode();
+    }
+}
+
+ enum Singleton {
+    SINGLETON;
+    Object o;
+
+    Singleton() {
+        this.o = new Object();
+    }
+
+    Object getInstance() {
+        return this.o;
     }
 }
