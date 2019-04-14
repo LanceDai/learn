3eb237791b2ad43b285a9ea0a200f2ac6a6aa67b
LanceDai <603842325@qq.com>
2019-04-14 23:00:15 +0800

    2019年04月14日

---
+++ scala_learn/src/scala/course7/StringApp.scala
@@ -0,0 +1,12 @@
+package course7
+
+/**
+  * @author LanceDai 
+  * @date 2019/4/14 09:46 
+  * @description * 
+  */
+object StringApp extends App {
+  val s = "Hello:"
+  val name = "PK"
+  println(s"Hello:$name")
+}
