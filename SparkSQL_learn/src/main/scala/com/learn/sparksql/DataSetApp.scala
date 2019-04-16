b4d5516ea81aa4a28dcdd56807753e1266587a55
LanceDai <603842325@qq.com>
2019-04-16 23:00:16 +0800

    2019年04月16日

---
+++ SparkSQL_learn/src/main/scala/com/learn/sparksql/DataSetApp.scala
@@ -0,0 +1,29 @@
+package com.learn.sparksql
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * @author LanceDai 
+  * @date 2019/4/16 15:17 
+  * @description DataSet操作
+  */
+object DataSetApp {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("dataSetApp")
+      .master("local[2]")
+      .getOrCreate()
+    //spark如何解析csv文件
+       // val path = "D:\\WorkSpace\\JavaWorkSpace\\learn\\SparkSQL_learn\\src\\main\\resources\\sales.csv"
+    var path = "/home/hadoop/data/sales.csv"
+    val df = spark.read.option("header", "true").option("inferSchema", "true").csv(path)
+    df.show()
+    import spark.implicits._
+    val ds = df.as[Sales]
+    ds.map(line=>line.itemId).show()
+    spark.stop()
+  }
+
+  case class Sales(transactionId: Int, customerId: Int, itemId: Int, amountPaid: Double)
+
+}
--- SparkSQL_learn/src/main/scala/com/learn/sparksql/SparkSQLThriftServerApp.scala
+++ SparkSQL_learn/src/main/scala/com/learn/sparksql/SparkSQLThriftServerApp.scala
@@ -2,12 +2,16 @@ package com.learn.sparksql
 
 import java.sql.{Connection, DriverManager}
 
+import org.apache.log4j.{LogManager, Logger}
+
 /**
   * @author LanceDai 
   * @date 2019/4/15 19:01 
   * @description 通过jdbc访问
   */
 object SparkSQLThriftServerApp {
+  val log: Logger = LogManager.getLogger(SparkSQLThriftServerApp.getClass)
+
   def main(args: Array[String]): Unit = {
     Class.forName("org.apache.hive.jdbc.HiveDriver")
     val conn: Connection = DriverManager.getConnection("jdbc:hive2://192.168.136.21:10000", "hadoop", "")
@@ -15,7 +19,7 @@ object SparkSQLThriftServerApp {
     val rs = pstmt.executeQuery()
     var count: Int = 0
     while (rs.next()) {
-      println(count + rs.getString("context"))
+      log.info(s"$count: ${rs.getString("context")}")
       count = count + 1
     }
     rs.close()
